7dd6a83dd6a6f3d059170fb825052da2be344f79
Allison Kaptur <allison.kaptur@gmail.com>
2013-09-08 20:12:02 -0400

    Mystifying curses behavior
    Why is KEY_UP not getting triggered by the up arrow?  Instead, I seem to get 'A', 'B', 'C', and 'D' from the arrow keys.

--- curses_experiment.py
+++ curses_experiment.py
@@ -1,21 +1,35 @@
 import curses
-from tetris import Tetris
+import string
+import random
 
+def letters():
+    for char in string.letters:
+        yield char
 
-
-def draw_screen(screen, board):
+def draw_screen(screen):
     begin_x = 20 ; begin_y = 7
     height = 25 ; width = 15
     win = curses.newwin(height, width, begin_y, begin_x)
+    # lets = letters()
 
-    str_board = board.draw_board_terminal()
-    win.addstr(str_board)
-    
-    win.refresh()
+    tick = 0
     while True:
-        pass
     
+        inp = win.getch()
+        mapper = {curses.KEY_UP:    'up   ',
+                  curses.KEY_DOWN:  'down ',
+                  curses.KEY_LEFT:  'left ',
+                  curses.KEY_RIGHT: 'right',
+
+        }
+        if inp in mapper:
+            char = mapper[inp]
+        else:
+            char = chr(inp)
+
+        win.addstr(0,0, char+"  ")
+        win.refresh()
+
 
 if __name__ == '__main__':
-    board = Tetris()
-    curses.wrapper(draw_screen, board)
+    curses.wrapper(draw_screen)
